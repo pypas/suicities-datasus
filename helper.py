978564b275daae6375d483582c3964d5f9890629
pypas <pypasofar@gmail.com>
2020-03-04 16:07:21 -0300

    First commit

---
+++ helper.py
@@ -0,0 +1,36 @@
+'''
+Helper functions to do EDA
+'''
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def categorical_summarized(dataframe, x=None, y=None, hue=None, palette='Set1', ax=None, order=None, verbose=True):
+    '''
+    Helper function that gives a quick summary of a given column of categorical data
+    Arguments
+    =========
+    dataframe: pandas dataframe
+    x: str. horizontal axis to plot the labels of categorical data, y would be the count
+    y: str. vertical axis to plot the labels of categorical data, x would be the count
+    hue: str. if you want to compare it another variable (usually the target variable)
+    palette: array-like. Colour of the plot
+    Returns
+    =======
+    Quick Stats of the data and also the count plot
+    '''
+    if x == None:
+        column_interested = y
+    else:
+        column_interested = x
+    series = dataframe[column_interested]
+    print(series.describe())
+    print('mode: ', series.mode())
+    if verbose:
+        print('='*80)
+        print(series.value_counts())
+
+    sns.countplot(x=x, y=y, hue=hue, data=dataframe,
+                  palette=palette, order=order, ax=ax)
+    plt.show()
