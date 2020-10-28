eb75bf8098fe539c9b2043907070f6b34abe65e2
Marcos Paulo <mpereiramoretti@gmail.com>
2020-10-28 11:08:45 -0300

    importing EDA dashboard working propely

--- Dashboard/EDA/eda_view.py
+++ Dashboard/EDA/eda_view.py
@@ -1,42 +1,52 @@
 import streamlit as st
-from eda_data import *
+from . import eda_data as dt
+import numpy as np
 
-"""
-# Análise exploratória de dados
-"""
+def present_eda():
+  st.markdown(
+    """
+    # Análise exploratória de dados
+    """
 
-"""
-Inicialmente, faremos uma análise exploratória dos dados contidos nas Declarações de Óbito do DATASUS.
-Foram selecionados os registros correspondentes às mortes por suicídio, que correspondem aos códigos CID-10 entre X60-X84.
+    """
+    Inicialmente, faremos uma análise exploratória dos dados contidos nas Declarações de Óbito do DATASUS.
+    Foram selecionados os registros correspondentes às mortes por suicídio, que correspondem aos códigos CID-10 entre X60-X84.
 
-As principais colunas foram extraídas, formando a tabela abaixo:
-"""
-suicide_df = get_suicide_data()
-st.write(suicide_df.head())
+    As principais colunas foram extraídas, formando a tabela abaixo:
+    """
+  )
 
-"""
-## Análise por data de óbito
-"""
+  suicide_df = dt.get_suicide_data()
+  st.write(suicide_df.head())
 
-"""
-Abaixo, pode-se observar o número de suicídios por data de óbito.
-"""
-plot_dtobito()
+  st.markdown(
+    """
+    ## Análise por data de óbito
+    """
 
+    """
+    Abaixo, pode-se observar o número de suicídios por data de óbito.
+    """
+  )
 
+  dt.plot_dtobito()
 
-"""
-## Análise por coluna
-"""
+  st.markdown(
+    """
+    ## Análise por coluna
+    """
+  )
 
-options = np.append(['Selecione uma análise'], ["Idade", "Sexo", "Estado Civil", "Raça/Cor", "Ocupação", "Escolaridade"])
-column = st.selectbox('Selecione uma análise:', options)
-plot_column(column)
+  options = np.append(['Selecione uma análise'], ["Idade", "Sexo", "Estado Civil", "Raça/Cor", "Ocupação", "Escolaridade"])
+  column = st.selectbox('Selecione uma análise:', options)
+  dt.plot_column(column)
 
-# """
-# ## Análise por município de residência
-# """
+  # st.markdown(
+  #    """
+  #    ## Análise por município de residência
+  #    """
+  # )
 
-# plot_codmunres()
+  # plot_codmunres()
 
-#plot_linha_ii()
+  # plot_linha_ii()
--- Dashboard/startup.py
+++ Dashboard/startup.py
@@ -1,13 +1,15 @@
 import streamlit as st
 from Homepage.homepage import present_homepage
+import EDA.eda_view as eda_vw
+import PySal.bivariate_view as moran
 
 st.sidebar.title("Dashboard")
 dashboard = st.sidebar.selectbox("Escolha o dashboard que deseja ver:", ["Homepage", "EDA", "Bivariate Moran's I", "Spearman"])
 if dashboard == "Homepage":
     present_homepage()
 elif dashboard == "EDA":
-    st.sidebar.success("EDA")
+    eda_vw.present_eda()
 elif dashboard == "Bivariate Moran's I":
-    st.sidebar.success("Bivariate Moran's I")
+    moran.present_moran()
 else:
     st.sidebar.success("Spearman")
